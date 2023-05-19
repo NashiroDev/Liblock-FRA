3dc5ceb958f332fe66b165e5f99b6afab87397af
Nathan <nathan.pauchon.38@gmail.com>
2023-05-19 15:58:28 +0200

    first commit

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return new Response("test.html");
+    }
+}
